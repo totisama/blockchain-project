30ea0cc59cd9edbab1edb74c48c1e44b39fea56f
ClaudioFoti <clau.foti@gmail.com>
2024-04-23 20:04:54 +0200

    Updated with changes made on main branch

--- blockchain.py
+++ blockchain.py
@@ -25,6 +25,11 @@ class BlockMessage:
     block: Block
     ttl: int = 3
 
+@dataclass(msg_id=3)
+class PeersMessage:
+    mid: bytes
+    ttl: int = 3
+
 
 class MyCommunity(Community):
     community_id = b'harbourspaceuniverse'
@@ -41,19 +46,28 @@ class MyCommunity(Community):
         self.blocks = []  # List to store finalized blocks
         self.current_block = Block('0')  # Current working block
 
+        self.known_peers_mid = set()
+
         self.add_message_handler(Transaction, self.on_transaction)
         self.add_message_handler(BlockMessage, self.receive_block)
+        self.add_message_handler(PeersMessage, self.receive_peers)
+
+        # Use any number as seed
+        random.seed(21)
 
     def started(self, id) -> None:
         logging.info('Community started')
+        self.known_peers_mid.add(self.my_peer.mid)
 
         # Testing purpose
-        if id == 1:
-            random_transaction_interval = random.randint(5, 10)
-            self.register_task("tx_create", self.create_transaction, delay=1, interval=random_transaction_interval)
+        # if id == 1:
+        random_transaction_interval = random.randint(5, 10)
+        self.register_task("tx_create", self.create_transaction, delay=7, interval=random_transaction_interval)
 
         random_check_interval = random.randint(5, 10)
-        self.register_task("check_txs", self.block_creation, delay=1, interval=random_check_interval)
+        self.register_task("check_txs", self.block_creation, delay=7, interval=random_check_interval)
+
+        self.register_task("send_peers", self.send_peers, delay=5)
 
     def peers_found(self):
         return len(self.get_peers()) > 0
@@ -66,7 +80,7 @@ class MyCommunity(Community):
             logging.info(f'[Node {self.get_peer_id(self.my_peer)}] No peers found')
             return
 
-        logging.info(f'[Node {self.get_peer_id(self.my_peer)}] Creating transaction')
+        # logging.info(f'[Node {self.get_peer_id(self.my_peer)}] Creating transaction')
         receiver_peer = random.choice([i for i in self.get_peers()])
 
         # ttl = 3 when creating a tx
@@ -74,37 +88,28 @@ class MyCommunity(Community):
         tx.public_key = self.crypto.key_to_bin(self.my_peer.key.pub())
         # should we sigh the hash of tx or the whole tx?
         tx.signature = self.crypto.create_signature(self.my_peer.key, tx.get_tx_bytes())
-        logging.info(
-            f'[Node {self.get_peer_id(self.my_peer)}] Sending transaction {tx.nonce} to {self.get_peer_id(receiver_peer)}')
+        # logging.info(
+        #     f'[Node {self.get_peer_id(self.my_peer)}] Sending transaction {tx.nonce} to {self.get_peer_id(receiver_peer)}')
         self.pending_txs[tx.get_tx_hash()] = tx
         # and this is correct that initially we send this tx only to receiver?
         self.ez_send(receiver_peer, tx)
         self.counter += 1
 
-        # WIP: We need this?
-        # if self.counter > self.max_messages:
-        #     self.cancel_pending_task("tx_create")
-        #     self.stop()
-        #     return
 
     def block_creation(self):
-        # WIP: get every known peer, not only the ones im connected to
-        peers = self.get_peers()
-        peers.append(self.my_peer)
-        selectedPeer = random.choice(peers)
+        selected_peer_mid = random.choice(list(self.known_peers_mid))
 
-        print(selectedPeer.mid, self.my_peer.mid)
-        if not selectedPeer.mid == self.my_peer.mid:
+        if not selected_peer_mid == self.my_peer.mid:
             return
 
-        logging.info(f'[Node {self.get_peer_id(self.my_peer)}] is creating a block')
+        # logging.info(f'[Node {self.get_peer_id(self.my_peer)}] is creating a block')
         for tx_hash in list(self.pending_txs.keys()):
             tx = self.pending_txs.pop(tx_hash)
             self.finalized_txs[tx_hash] = tx
             self.current_block.add_transaction(tx)
 
             if self.current_block.is_full():
-                logging.info(f'[Node {self.get_peer_id(self.my_peer)}] Chosen one')
+                logging.info(f'[Node {self.get_peer_id(self.my_peer)}] is creating a block {len(self.blocks)}')
                 self.finalize_and_broadcast_block()
                 break
 
@@ -136,29 +141,46 @@ class MyCommunity(Community):
 
     @lazy_wrapper(BlockMessage)
     async def receive_block(self, peer: Peer, payload: BlockMessage) -> None:
-        print('----------on block----------')
-        print(payload)
+        logging.info('----------on block----------')
 
-        print(payload.block.previous_hash)
-        print(payload.block.merkle_hash)
-        print(payload.block.transactions)
+        # If the block is already our chain we do nothing
+        if payload.hash in [block.get_merkle_hash() for block in self.blocks]:
+            logging.info('block already known')
 
-        # Check if the block is already in the chain
-        # if payload.hash not in [block.get_merkle_hash() for block in self.blocks]:
-        #     print(f'Block {payload.hash} not in my chain {self.get_peer_id(peer)}')
-        #     logging.info(f'Block {payload.hash} not in my chain {self.get_peer_id(peer)}')
-        #     self.blocks.append(payload.block)
+            return
 
-        # Remove block transactions from my pending_txs list
-        # for tx in unserialized_block.transactions:
-        #     if (tx.sender, tx.nonce) in [(tx.sender, tx.nonce) for tx in self.pending_txs]:
-        #         self.pending_txs.remove(tx)
+        # Check block transactions and remove them from pending_txs list
+        block_transactions = [self.serializer.unpack_serializable(Transaction, tx)[0] for tx in payload.block.transactions]
+        new_balances = self.balances.copy()
+        valid_txs = []
+
+        for tx in block_transactions:
+            tx_hash = tx.get_tx_hash()
+
+            if new_balances[tx.sender] - tx.amount >= 0:
+                new_balances[tx.sender] -= tx. amount
+                new_balances[tx.receiver] += tx.amount
+
+                if tx_hash in self.pending_txs:
+                    self.pending_txs.pop(tx_hash)
+
+                valid_txs.append(tx.get_tx_bytes())
+
+        payload.block.set_transactions(valid_txs)
+        self.balances = new_balances
+        self.blocks.append(payload.block)
+
+        if payload.ttl > 0:
+            payload.ttl -= 1
+
+            get_peers_to_distribute = random.sample(self.get_peers(), min(k, len(self.get_peers())))
+            for peer in get_peers_to_distribute:
+                self.ez_send(peer, payload)
 
     def finalize_and_broadcast_block(self):
         self.current_block.update_tree()
         new_block_hash = self.current_block.get_merkle_hash()
-        print(f'New block hash: {new_block_hash}')
-        logging.info(f'New block hash: {new_block_hash}')
+        # logging.info(f'New block hash: {new_block_hash}')
         self.blocks.append(self.current_block)
 
         self.broadcast_block(new_block_hash, self.current_block)
@@ -169,3 +191,24 @@ class MyCommunity(Community):
 
         for peer in self.get_peers():
             self.ez_send(peer, blockMessage)
+
+
+    def send_peers(self) -> None:
+        peers = self.get_peers()
+        peerMessage = PeersMessage(self.my_peer.mid)
+
+        for peer in peers:
+            self.ez_send(peer, peerMessage)
+
+
+    @lazy_wrapper(PeersMessage)
+    def receive_peers(self, peer: Peer, payload: PeersMessage) -> None:
+        if payload.mid not in self.known_peers_mid:
+
+            self.known_peers_mid.add(payload.mid)
+
+            peers = self.get_peers()
+            peerMessage = PeersMessage(payload.mid, payload.ttl - 1)
+
+            for peer in peers:
+                self.ez_send(peer, peerMessage)
