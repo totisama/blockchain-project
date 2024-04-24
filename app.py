e171986f4a45cde2141c570edc7a450cc98155ff
ClaudioFoti <clau.foti@gmail.com>
2024-04-24 21:03:55 +0200

    Created streamlit file with ipv8 instance in it + structure change

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+from asyncio import run
+
+from ipv8.configuration import ConfigBuilder, Strategy, WalkerDefinition, default_bootstrap_defs
+from ipv8_service import IPv8
+
+from blockchain import MyCommunity
+
+
+async def create_community():
+    builder = ConfigBuilder().clear_keys().clear_overlays()
+    builder.add_key("my peer", "medium", f"ec{7}.pem")
+    builder.add_overlay("MyCommunity", "my peer",
+                        [WalkerDefinition(Strategy.RandomWalk,
+                                          10, {'timeout': 3.0})],
+                        default_bootstrap_defs, {}, [('started', 7)])
+    ipv8_instance = IPv8(builder.finalize(),
+                         extra_communities={'MyCommunity': MyCommunity})
+    return ipv8_instance
+
+
+async def start_community(ipv8_instance):
+    await ipv8_instance.start()
+
+
+ipv8_instance = run(create_community())
+run(start_community(ipv8_instance))
+community = ipv8_instance.overlays[0]
+
+print(community.my_peer.mid)
+print(community.community_id)
+print(community.get_peers())
+
+
+def onclickbtn():
+    tx = community.create_transaction()
+    if tx is not None:
+        st.write(tx.sender)
+
+
+st.write("""
+# My first app
+Hello *world!*
+""")
+
+st.button("Test", on_click=onclickbtn)
